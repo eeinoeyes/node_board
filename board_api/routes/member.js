d0c0fdd25611de3141c9b6c31bf17e11f37f020c
eeinoeyes <loveourjune@gmail.com>
2025-07-14 10:23:34 +0900

    up

--- board_api/routes/member.js
+++ board_api/routes/member.js
@@ -3,10 +3,11 @@ const passport = require('passport')
 const bcrypt = require('bcrypt')
 const router = express.Router()
 const Member = require('../models/member')
+const { isNotLoggedIn, isLoggedIn } = require('./middleware')
 
 //회원가입
 //localhost:8000/member/enter
-router.post('/enter', async (req, res, next) => {
+router.post('/enter', isNotLoggedIn, async (req, res, next) => {
    try {
       const { email, name, password } = req.body
       const exMember = await Member.findOne({ where: { email } })
@@ -41,20 +42,51 @@ router.post('/enter', async (req, res, next) => {
 })
 
 //로그인
-router.post('/login', async (req, res, next) => {
-   try {
-   } catch (err) {
-      err.message = '로그인에 실패했습니다.'
-      next(err)
-   }
+router.post('/login', isNotLoggedIn, async (req, res, next) => {
+   passport.authenticate('local', (error, member, info) => {
+      if (error) {
+         error.status = 500
+         error.message = '인증 중 오류 발생'
+         return next(error)
+      }
+      if (!member) {
+         const error = new Error(info.message || '로그인 실패')
+         error.status = 401
+         return next(error)
+      }
+
+      req.login(member, (loginError) => {
+         if (loginError) {
+            loginError.status = 500
+            loginError.message = '로그인 중 오류 발생'
+            return next(loginError)
+         }
+      })
+
+      res.status(200).json({
+         success: true,
+         message: '로그인 성공',
+         member: {
+            id: member.id,
+            name: member.name,
+         },
+      })
+   })(req, res, next)
 })
 
 //로그아웃
-router.get('/logout', async (req, res, next) => {
-   try {
-   } catch (err) {
-      err.message = '로그아웃 도중 오류가 발생했습니다.'
-      next(err)
-   }
+router.get('/logout', isLoggedIn, async (req, res, next) => {
+   req.logOut((logoutError) => {
+      if (logoutError) {
+         logoutError.status = 500
+         logoutError.message = '로그아웃 중 오류 발생'
+         return next(logoutError)
+      }
+
+      res.status(200).json({
+         success: true,
+         message: '로그아웃 완료',
+      })
+   })
 })
 module.exports = router
--- board_api/routes/middleware.js
+++ board_api/routes/middleware.js
@@ -1,4 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+exports.isLoggedIn = (req, res, next) => {
+   if (req.isAuthenticated()) {
+      next()
+   } else {
+      const error = new Error('로그인이 필요합니다.')
+      error.status = 403
+      return next(error)
+   }
+}
+
+exports.isNotLoggedIn = (req, res, next) => {
+   if (!req.isAuthenticated()) {
+      next()
+   } else {
+      const error = new Error('이미 로그인 된 상태입니다.')
+      error.status = 400
+      return error(err)
+   }
+}
+
 module.exports = router
